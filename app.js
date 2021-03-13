03081f1aa5a17f803c837bc6282a79912d76f09e
JacobNewton007 <metikenny@gmail.com>
2021-03-13 07:14:16 +0100

    compile the statics

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
+// global function 
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
+// initializing the objects
 const app = express()
+
+// self define function
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+const dirRoot = require('./util/path')
+
+
+// init body parser
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')));
 
+// set the routers -- i
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product", method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(dirRoot, 'views', '404.html'))
 })
 
-
-
-
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>hello from express</h1>')
-});
-
-
-app.listen(3000);
+app.listen(3001);
